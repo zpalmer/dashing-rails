7a621d5a697e537b5a3ab8e686af0d5a8d2cb103
Zoe Palmer <zoe.palmer@getbraintree.com>
2018-02-16 18:25:30 +0000

    Unsubscribe from redis channel after timeout and retry 5 times

--- app/controllers/dashing/events_controller.rb
+++ app/controllers/dashing/events_controller.rb
@@ -3,30 +3,35 @@ module Dashing
     include ActionController::Live
 
     def index
-      @redis = Dashing.redis
+      begin
+        @redis = Dashing.redis
 
-      response.headers['Content-Type']      = 'text/event-stream'
-      response.headers['X-Accel-Buffering'] = 'no'
-      response.stream.write latest_events
+        response.headers['Content-Type']      = 'text/event-stream'
+        response.headers['X-Accel-Buffering'] = 'no'
+        response.stream.write latest_events
 
-      @redis.with do |redis_connection|
-        redis_connection.psubscribe("#{Dashing.config.redis_namespace}.*") do |on|
-          on.pmessage do |pattern, event, data|
-            response.stream.write("data: #{data}\n\n")
+        redis_retry = 0
+        while redis_retry < 5
+          @redis.with do |redis_connection|
+            redis_connection.psubscribe_with_timeout(150, "#{Dashing.config.redis_namespace}.*") do |on|
+              on.pmessage do |pattern, event, data|
+                response.stream.write("data: #{data}\n\n")
+              end
+            end
           end
         end
+      rescue IOError, ActionController::Live::ClientDisconnected
+        logger.info "[Dashing][#{Time.now.utc.to_s}] Stream closed"
+      ensure
+        Dashing.shutdown_redis
+        response.stream.close
       end
-    rescue IOError, ActionController::Live::ClientDisconnected
-      logger.info "[Dashing][#{Time.now.utc.to_s}] Stream closed"
-    ensure
-      Dashing.shutdown_redis
-      response.stream.close
-    end
 
-    def latest_events
-      @redis.with do |redis_connection|
-        events = redis_connection.hvals("#{Dashing.config.redis_namespace}.latest")
-        events.map { |v| "data: #{v}\n\n" }.join
+      def latest_events
+        @redis.with do |redis_connection|
+          events = redis_connection.hvals("#{Dashing.config.redis_namespace}.latest")
+          events.map { |v| "data: #{v}\n\n" }.join
+        end
       end
     end
   end
