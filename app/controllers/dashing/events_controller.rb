b7805c310c0f6656dd6d45d9155d5377b1ba1145
Zoe Palmer <zoe.palmer@getbraintree.com>
2018-02-27 17:50:09 +0000

    Infinite redis psub with timeout loop

--- app/controllers/dashing/events_controller.rb
+++ app/controllers/dashing/events_controller.rb
@@ -10,28 +10,28 @@ module Dashing
         response.headers['X-Accel-Buffering'] = 'no'
         response.stream.write latest_events
 
-        redis_retry = 0
-        while redis_retry < 5
+        loop do
           @redis.with do |redis_connection|
-            redis_connection.psubscribe_with_timeout(150, "#{Dashing.config.redis_namespace}.*") do |on|
+            redis_connection.psubscribe_with_timeout(61, "#{Dashing.config.redis_namespace}.*") do |on|
               on.pmessage do |pattern, event, data|
                 response.stream.write("data: #{data}\n\n")
               end
             end
           end
         end
-      rescue IOError, ActionController::Live::ClientDisconnected
+      rescue IOError, ActionController::Live::ClientDisconnected => error
+        logger.error(error)
         logger.info "[Dashing][#{Time.now.utc.to_s}] Stream closed"
       ensure
         Dashing.shutdown_redis
         response.stream.close
       end
+    end
 
-      def latest_events
-        @redis.with do |redis_connection|
-          events = redis_connection.hvals("#{Dashing.config.redis_namespace}.latest")
-          events.map { |v| "data: #{v}\n\n" }.join
-        end
+    def latest_events
+      @redis.with do |redis_connection|
+        events = redis_connection.hvals("#{Dashing.config.redis_namespace}.latest")
+        events.map { |v| "data: #{v}\n\n" }.join
       end
     end
   end
